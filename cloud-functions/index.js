2bbdb75972fff272a62961d6d206fee634002ceb
Takashi Chiba <work@tachiba.jp>
2019-07-10 19:53:52 +0900

    🐛 Avoid to ignore dist/

--- cloud-functions/index.js
+++ cloud-functions/index.js
@@ -1,9 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mrm_core_1 = require("mrm-core");
+const COPY_FILES = [
+    // Cloud Function may create a default file which ignores dist/
+    ".gcloudignore",
+];
 const packages = ["@google-cloud/functions-framework"];
 const devPackages = ["@types/express"];
 function task(config) {
+    mrm_core_1.copyFiles(__dirname, COPY_FILES);
     mrm_core_1.json("tsconfig.json")
         .merge({
         compilerOptions: {
--- cloud-functions/index.ts
+++ cloud-functions/index.ts
@@ -1,10 +1,17 @@
-import { install, json } from "mrm-core";
+import { copyFiles, install, json } from "mrm-core";
 import { Config } from "../lib/config";
+
+const COPY_FILES = [
+  // Cloud Function may create a default file which ignores dist/
+  ".gcloudignore",
+];
 
 const packages = ["@google-cloud/functions-framework"];
 const devPackages = ["@types/express"];
 
 function task(config: Config) {
+  copyFiles(__dirname, COPY_FILES);
+
   json("tsconfig.json")
     .merge({
       compilerOptions: {
