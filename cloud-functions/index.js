eb0823a2cabf6986e2eb5e4f1abfdfecf5988a89
Takashi Chiba <work@tachiba.jp>
2019-07-10 19:10:45 +0900

    🐛 function-framework should be runtime dependency

--- cloud-functions/index.js
+++ cloud-functions/index.js
@@ -1,9 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mrm_core_1 = require("mrm-core");
-const packages = ["@google-cloud/functions-framework", "@types/express"];
+const packages = ["@google-cloud/functions-framework"];
+const devPackages = ["@types/express"];
 function task(config) {
-    mrm_core_1.install(packages);
+    mrm_core_1.install(packages, {
+        dev: false
+    });
+    mrm_core_1.install(devPackages, {
+        dev: true
+    });
 }
 task.description = "Add Cloud Function (runtime>=nodejs10)";
 module.exports = task;
--- cloud-functions/index.ts
+++ cloud-functions/index.ts
@@ -1,10 +1,17 @@
 import { install } from "mrm-core";
 import { Config } from "../lib/config";
 
-const packages = ["@google-cloud/functions-framework", "@types/express"];
+const packages = ["@google-cloud/functions-framework"];
+const devPackages = ["@types/express"];
 
 function task(config: Config) {
-  install(packages);
+  install(packages, {
+    dev: false
+  });
+
+  install(devPackages, {
+    dev: true
+  });
 }
 
 task.description = "Add Cloud Function (runtime>=nodejs10)";
