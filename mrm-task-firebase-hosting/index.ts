cfb646244a5d5cfb9fb00668cc098dabed46bb92
Takashi Chiba <work@tachiba.jp>
2021-08-10 19:27:48 +0900

    chore: deprecate unused packages

--- mrm-task-firebase-hosting/index.ts
+++
@@ -1,8 +0,0 @@
-import { packageJson } from "mrm-core";
-
-function task() {
-  packageJson().appendScript("deploy", "firebase deploy --only hosting").save();
-}
-task.description = "Add Firebase Hosting";
-
-module.exports = task;
